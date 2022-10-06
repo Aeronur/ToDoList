a7602a0549708b738ca93e1f7b67f64fdb3c7949
Nur <aeronur93@gmail.com>
2022-10-06 12:10:59 +0400

    Init todolist

---
+++ ToDoListt/ToDoListtApp.swift
@@ -0,0 +1,25 @@
+//
+//  ToDoListtApp.swift
+//  ToDoListt
+//
+//  Created by Nurullo Ergashev on 21/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    
+    @StateObject var listViewModel: ListViewModel = ListViewModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+            ListView()
+        }
+            .navigationViewStyle(StackNavigationViewStyle())
+            .environmentObject(listViewModel)
+    }
+}
+    
+}
---
+++ ToDoListt/ViewModels/ListViewModel.swift
@@ -0,0 +1,76 @@
+//
+//  ListViewModel.swift
+//  ToDoListt
+//
+//  Created by Nurullo Ergashev on 21/06/2022.
+//
+
+import Foundation
+
+/*
+ CRUD FUNCTION
+ 
+ Create
+ Read
+ Update
+ Delete
+ 
+ */
+
+import FileProvider
+
+class ListViewModel: ObservableObject {
+    
+    @Published var items: [ItemModel] = [] {
+        
+        didSet {
+            saveItems()
+        }
+    }
+    
+    
+    let itemsKey: String = "items_list"
+    
+    init() {
+        getItems()
+    }
+   
+    func getItems() {
+        
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedIdtems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else {return}
+        self.items = savedIdtems
+    }
+    
+    func deleteItem(indexSet: IndexSet) {
+        items.remove(atOffsets: indexSet)
+    }
+    
+    func moveItem(from: IndexSet, to: Int) {
+        items.move(fromOffsets: from, toOffset: to)
+    }
+    
+    func addItem(title: String) {
+        
+        let newItem = ItemModel(title: title, isCompleted: false)
+        items.append(newItem)
+    }
+    
+    func updateItem(item: ItemModel) {
+
+        
+        if let index = items.firstIndex(where: { $0.id == item.id })
+        { items[index] = item.updateCompletion()
+            
+        }
+
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
+        }
+    }
+}
